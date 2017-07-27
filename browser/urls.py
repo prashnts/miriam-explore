2f95991913bed85fe35dcc2f99cffb4b8f9cf517
Prashant Sinha <prashant+git@noop.pw>
2017-07-27 22:10:21 +0530

    Update root tempaltes and fix url for browser

--- browser/urls.py
+++ browser/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$',
-        TemplateView.as_view(template_name='browser/index.html.html'),
+        TemplateView.as_view(template_name='browser/index.html'),
         name='browser/index.html'),
 ]

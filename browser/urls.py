98f2804e6775590f290294a77b1b88788fcc9496
Prashant Sinha <prashant+git@noop.pw>
2017-10-13 14:35:29 +0200

    Redirect browser to homepage because we dont want to add yet another page

--- browser/urls.py
+++ browser/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='browser/index.html'), name='browser_home'),
+    url(r'^$', RedirectView.as_view(url='/'), name='browser_home'),
     url(r'^network/$', TemplateView.as_view(template_name='browser/network.html'), name='browser_network'),
     url(r'^heatmap/$', TemplateView.as_view(template_name='browser/heatmap.html'), name='browser_heatmap'),
     url(r'^sankey/$', views.sankey, name='browser_sankey'),

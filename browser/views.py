fb454e552a7bf609258e8f20726010b77b6d4482
Prashant Sinha <prashant+git@noop.pw>
2017-10-31 20:43:25 +0100

    Render table html to tables

--- browser/views.py
+++ browser/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
+import pandas as pd
+import math
+
+from annoying.decorators import render_to
 
 from browser.utils import get_fixture
 
+DEFAULT_SAMPLE_ID = 'emtab2919-adrenal_gland'
+SAMPLE_CHUNK_SIZE = 500
 
+experiments = get_fixture('browser.exp--tissues.yaml')
+
+
+@render_to('browser/sankey.html')
 def sankey(request):
-    expts = get_fixture('browser.exp--tissues.yaml')
-
-    context = {
-      'experiments': expts,
+    return {
+      'experiments': experiments,
     }
 
-    return render(request, 'browser/sankey.html', context)
 
+@render_to('browser/tables.html')
+def tables(request, sample_id=DEFAULT_SAMPLE_ID, page=1):
+    if page < 1:
+        page = 1
+
+    sample = pd.read_pickle('/Users/prashantsinha/.miriam/pickles/{}.pkl'.format(sample_id))
+
+    chunk = slice(SAMPLE_CHUNK_SIZE * (page - 1), SAMPLE_CHUNK_SIZE * page)
+
+    table = sample.loc[chunk].to_html(border=0, classes='pt-table pt-striped pt-condensed np-table')
+    return {
+      'sample_id': sample_id,
+      'current': page,
+      'page_max': math.ceil(len(sample) / SAMPLE_CHUNK_SIZE),
+
+      'experiments': experiments,
+      'table': table,
+    }

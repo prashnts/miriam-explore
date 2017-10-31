5cab198601630a19846c0eb727a13d58a1d5e593
Prashant Sinha <prashant+git@noop.pw>
2017-10-31 20:42:59 +0100

    Add entry for browser_tables

--- browser/urls.py
+++ browser/urls.py
@@ -1,11 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/'), name='browser_home'),
-    url(r'^network/$', TemplateView.as_view(template_name='browser/network.html'), name='browser_network'),
-    url(r'^heatmap/$', TemplateView.as_view(template_name='browser/heatmap.html'), name='browser_heatmap'),
-    url(r'^sankey/$', views.sankey, name='browser_sankey'),
+    path('/', RedirectView.as_view(url='/'), name='browser_home'),
+    path('network/', TemplateView.as_view(template_name='browser/network.html'), name='browser_network'),
+    path('heatmap/', TemplateView.as_view(template_name='browser/heatmap.html'), name='browser_heatmap'),
+    path('sankey/', views.sankey, name='browser_sankey'),
+
+    path('tables/', views.tables, name='browser_tables'),
+    path('tables/<str:sample_id>/', views.tables, name='browser_tables'),
+    path('tables/<str:sample_id>/<int:page>/', views.tables, name='browser_tables'),
 ]

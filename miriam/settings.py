cadeeb93f15163e83bb0bd9c37f27dd506bb82b7
Prashant Sinha <prashant+git@noop.pw>
2017-12-05 16:58:00 +0100

    Remove passwords

--- miriam/settings.py
+++ miriam/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'miriam.basicauth.naive_lockdown_middleware',
+    # 'miriam.basicauth.naive_lockdown_middleware',
 ]
 
 ROOT_URLCONF = 'miriam.urls'
